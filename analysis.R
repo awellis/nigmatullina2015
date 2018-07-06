99ff88e4d8a92cfd9853627c4b4f75975100dd17
Andrew Ellis <a.w.ellis@gmail.com>
2018-07-06 16:52:05 +0200

    data set should now be ok

--- analysis.R
+++ analysis.R
@@ -25,52 +25,65 @@ NigmatullinaData$imagery <- factor(NigmatullinaData$imagery,
 
 ## -----
 
-NigmatullinaData %>% ggplot(aes(x = rt, fill = imagery)) +
+p_rt <- NigmatullinaData %>% ggplot(aes(x = rt, fill = imagery)) +
     # geom_histogram(aes(y = ..density..), binwidth = 1, alpha = 0.8) +
     geom_density(alpha = 0.8) +
-    facet_wrap(~participant)
+    facet_wrap(~participant) +
+    scale_fill_viridis_d() +
+    theme_bw()
+p_rt
 
-NigmatullinaData %>% ggplot(aes(x = nystagmus, fill = imagery)) +
+p_nystagmus <- NigmatullinaData %>% ggplot(aes(x = nystagmus, fill = imagery)) +
     # geom_histogram(aes(y = ..density..), binwidth = 1, alpha = 0.8) +
     geom_density(alpha = 0.8) +
-    facet_wrap(~participant)
+    facet_wrap(~participant) +
+    scale_fill_viridis_d() +
+    theme_bw()
+p_nystagmus
+## -----
 
+std_error <- function(x) {
+    sd(x)/sqrt(length(x))
+}
 
 RTSummary <- NigmatullinaData %>%
     select(participant, imagery, rt) %>%
     drop_na() %>%
     group_by(participant, imagery) %>%
-    summarize(mean = mean(rt),
-              sd = sd(rt))
+    summarise_all(funs(mean, median, sd, std_error))
 
 VORSummary <- NigmatullinaData %>%
     select(participant, imagery, nystagmus) %>%
     drop_na() %>%
     group_by(participant, imagery) %>%
-    summarise(mean = mean(nystagmus),
-              sd = sd(nystagmus))
-
+    summarise_all(funs(mean, median, sd, std_error))
 
 ImageryScoreSummary <- NigmatullinaData %>%
     select(participant, imagery, imagery_score) %>%
-    drop_na() %>%
     group_by(participant, imagery) %>%
-    # summarise_all(funs(mean,sd, plotrix::std.error))
+    summarise_all(funs(mean, median, sd, std_error)) %>%
+    drop_na()
 
-    summarise(mean = round(mean(imagery_score), 2))
 
 ProportionCorrect <- NigmatullinaData %>%
     select(participant, imagery, correct) %>%
     drop_na() %>%
     group_by(participant, imagery) %>%
-    summarise(mean = mean(correct),
-              sd = sd(correct))
+    summarise_all(funs(mean, sd, std_error))
 
+## ----
 
-
-RTSummary %>% ggplot(aes(x = imagery, y = mean, color = participant)) +
-    geom_point() +
+RTSummary %>% ggplot(aes(x = imagery, y = mean,
+                         color = participant)) +
+    geom_errorbar(aes(ymin = mean-std_error,
+                      ymax = mean+std_error),
+                  width = 0.1,
+                  position = "identity") +
     geom_line(aes(group = participant))
+
+# RTSummary %>%
+#     ggplot(aes(x = mean, fill = imagery)) +
+#     geom_histogram(position = "identity", alpha = 0.6)
 
 
 VORSummary %>% ggplot(aes(x = imagery,
@@ -87,6 +100,36 @@ ImageryScoreSummary %>% ggplot(aes(x = imagery, y = mean,
 
 
 
+ProportionCorrect %>%
+    # filter(participant != 75) %>%
+    ggplot(aes(x = imagery, y = mean,
+                                   color = participant)) +
+    geom_point() +
+    geom_line(aes(group = participant))
+
+# we should probably remove participant 75
 
 
+## Nigmatullina figures ----
 
+Summary <- RTSummary %>%
+    group_by(imagery) %>%
+    summarise(mu = mean(mean), se = std_error(mean))
+
+Summary %>% ggplot(aes(x = imagery, y = mu,
+                       fill = imagery)) +
+    geom_bar(position=position_dodge(), stat="identity") +
+    geom_errorbar(aes(ymin = mu-se,
+                      ymax = mu+se),
+                  width = 0.1,
+                  position = "dodge") +
+    # geom_line(aes(group = 1)) +
+    # scale_color_viridis_d() +
+    scale_fill_viridis_d() +
+    theme_bw()
+
+
+## brms models ----
+
+fit <- brm(rt ~ imagery)
+NigmatullinaData
