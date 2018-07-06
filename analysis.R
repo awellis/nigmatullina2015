1789c7d3cdaa33057e5f595acd611bb422b7998a
Andrew Ellis <a.w.ellis@gmail.com>
2018-07-06 15:42:16 +0200

    fix coding of imagery condition

--- analysis.R
+++ analysis.R
@@ -3,8 +3,8 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-colorPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
-                  "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+# colorPalette <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
+                  # "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 
 ## load data ----
 load("data/nigmatullina.Rda")
